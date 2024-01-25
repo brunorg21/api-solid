7948bc05017c74fbeeed1c9f3966b3e9746aa2a1
brunorg21 <rbrunog21@gmail.com>
2024-01-25 08:38:37 -0300

    feat: unit test for get-user-profile use-case

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,39 @@
+import { expect, describe, it, beforeEach } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+
+import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { GetUserProfileUseCase } from "./get-user-profile";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfileUseCase;
+describe("authenticate use case", async () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(usersRepository);
+  });
+
+  it("should be able to get user profile", async () => {
+    const createdUser = await usersRepository.create({
+      email: "jhondoe@email.com",
+      name: "Jhon Doe",
+      password_hash: await hash("123", 6),
+    });
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+    expect(user.name).toEqual("Jhon Doe");
+  });
+
+  it("should not be able to get user profile with wrong id", async () => {
+    expect(() =>
+      sut.execute({
+        userId: "non-existing-id",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
